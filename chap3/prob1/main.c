78eb88027848746207431857e1c5068c5ac03f38
lDrctl <sonms3275@gmail.com>
2026-09-23 02:50:30 +0000

    asdasd

--- chap3/prob1/main.c
+++ chap3/prob1/main.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
+#include "copy.h"
 
-int main() {
-	printf("Hello World!\n");
-		return 0;
+char line[MAXLINE]; 
+char longest[MAXLINE]; 
+
+int main(){
+  int len;
+  int max;
+  max = 0;
+
+  while(gets(line) != NULL) {
+    len = strlen(line);
+
+    if(len > max) {
+       max = len;
+       copy(line, longest);
+      }
+    }
+
+  if(max > 0) 
+    printf("%s \n", longest);
+
+  return 0;
 }
+
